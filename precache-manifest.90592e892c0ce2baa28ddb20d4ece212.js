a2c8c9efee380d6247ab8ac7a3bebc7fabec7f5a
abazunts home <bazunc@gmail.com>
2019-09-25 22:51:30 +0500

    Updates

---
+++ precache-manifest.90592e892c0ce2baa28ddb20d4ece212.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b49310421e86ec7a789c14b94b8a8c1",
+    "url": "/fe-ipr/index.html"
+  },
+  {
+    "revision": "40c1ec8c20cba236af56",
+    "url": "/fe-ipr/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "e4c38a734c441968dd44",
+    "url": "/fe-ipr/static/css/main.de7f23f4.chunk.css"
+  },
+  {
+    "revision": "40c1ec8c20cba236af56",
+    "url": "/fe-ipr/static/js/2.10bde98d.chunk.js"
+  },
+  {
+    "revision": "e4c38a734c441968dd44",
+    "url": "/fe-ipr/static/js/main.b1951ffe.chunk.js"
+  },
+  {
+    "revision": "a77a76e6a26d6289932a",
+    "url": "/fe-ipr/static/js/runtime~main.6f84fff9.js"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "/fe-ipr/static/media/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.d7e60f9d.woff2"
+  },
+  {
+    "revision": "34cf6f06e7e590af54547fb04357406b",
+    "url": "/fe-ipr/static/media/saip-logo.34cf6f06.png"
+  }
+]);
